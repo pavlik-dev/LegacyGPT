12651fb7683545e4bca3e02c08f81b45d4e91e37
Pavlik <pavliktt@pyt.pp.ua>
2025-06-03 22:06:46 +0300

    ACTUALLY remove Api and remove api key check

--- src/components/Menu/MenuOptions/Api.tsx
+++
@@ -1,4 +0,0 @@
-const Config = () => {
-};
-
-export default Config;
--- src/hooks/useSubmit.ts
+++ src/hooks/useSubmit.ts
@@ -26,9 +26,9 @@ const useSubmit = () => {
     try {
       if (!apiKey || apiKey.length === 0) {
         // official endpoint
-        if (apiEndpoint === officialAPIEndpoint) {
-          throw new Error(t('noApiKeyWarning') as string);
-        }
+        // if (apiEndpoint === officialAPIEndpoint) {
+        //   throw new Error(t('noApiKeyWarning') as string);
+        // }
 
         // other endpoints
         data = await getChatCompletion(
