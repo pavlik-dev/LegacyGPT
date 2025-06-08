48491880f1ed47328e28921accd45ea721353202
Pavlik <pavliktt@pyt.pp.ua>
2025-06-09 00:15:23 +0300

    add change model button

--- src/components/Chat/ChatContent/ChatTitle.tsx
+++ src/components/Chat/ChatContent/ChatTitle.tsx
@@ -5,6 +5,8 @@ import useStore from '@store/store';
 import ConfigMenu from '@components/ConfigMenu';
 import { ChatInterface, ConfigInterface } from '@type/chat';
 import { _defaultChatConfig, modelNames } from '@constants/chat';
+import BaseButton from './Message/View/Button/BaseButton';
+import EditIcon2 from '@icon/EditIcon2';
 
 const ChatTitle = React.memo(() => {
   const { t } = useTranslation('model');
@@ -43,29 +45,19 @@ const ChatTitle = React.memo(() => {
   return config ? (
     <>
       <div
-        className='flex gap-x-4 gap-y-1 flex-wrap w-full items-center justify-center border-b border-black/10 bg-gray-50 p-3 dark:border-gray-900/50 dark:bg-gray-700 text-gray-600 dark:text-gray-300'
-        // onClick={() => {
-        //   setIsModalOpen(true);
-        // }}
+        className='relative flex w-full border-b border-black/10 bg-gray-50 p-3 dark:border-gray-900/50 dark:bg-gray-700 text-gray-600 dark:text-gray-300'
       >
-        <div className='text-center'>
-          {t('model')}: {modelNames[config.model]}
+        <div className='flex-grow flex justify-center items-center'>
+          <div className='text-center'>
+            {t('model')}: {modelNames[config.model]}
+          </div>
         </div>
-        {/* <div className='text-center p-1 rounded-md bg-gray-300/20 dark:bg-gray-900/10 hover:bg-gray-300/50 dark:hover:bg-gray-900/50'>
-          {t('token.label')}: {config.max_tokens}
+        <div 
+          onClick={() => setIsModalOpen(true)} 
+          className='cursor-pointer absolute right-3 top-1/2 -translate-y-1/2 p-1 rounded-md bg-gray-300/20 dark:bg-gray-900/10 hover:bg-gray-300/50 dark:hover:bg-gray-900/50'
+        >
+          <EditIcon2 />
         </div>
-        <div className='text-center p-1 rounded-md bg-gray-300/20 dark:bg-gray-900/10 hover:bg-gray-300/50 dark:hover:bg-gray-900/50'>
-          {t('temperature.label')}: {config.temperature}
-        </div>
-        <div className='text-center p-1 rounded-md bg-gray-300/20 dark:bg-gray-900/10 hover:bg-gray-300/50 dark:hover:bg-gray-900/50'>
-          {t('topP.label')}: {config.top_p}
-        </div>
-        <div className='text-center p-1 rounded-md bg-gray-300/20 dark:bg-gray-900/10 hover:bg-gray-300/50 dark:hover:bg-gray-900/50'>
-          {t('presencePenalty.label')}: {config.presence_penalty}
-        </div>
-        <div className='text-center p-1 rounded-md bg-gray-300/20 dark:bg-gray-900/10 hover:bg-gray-300/50 dark:hover:bg-gray-900/50'>
-          {t('frequencyPenalty.label')}: {config.frequency_penalty}
-        </div> */}
       </div>
       {isModalOpen && (
         <ConfigMenu
